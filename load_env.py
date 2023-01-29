4d17f7f481ff00f6a00ce6db3ba21e387e3e1538
Andrew Kraevskii <andrew.kraevskii@gmail.com>
2023-01-30 02:31:43 +0300

    update load_env

--- load_env.py
+++ load_env.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_TOKEN = getenv("API_TOKEN") or ""
+API_TOKEN = getenv("API_TOKEN", "")
 if API_TOKEN is "":
     raise ValueError("API_TOKEN is not set")
 
-GOOGLE_TOKEN = getenv("GOOGLE_TOKEN") or ""
+GOOGLE_TOKEN = getenv("GOOGLE_TOKEN", "")
 if GOOGLE_TOKEN is "":
     raise ValueError("GOOGLE_TOKEN is not set")
