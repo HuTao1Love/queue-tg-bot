86425da11758bfa74524e0f96e7163514932470f
HuTao <zakhar.yagodin@yandex.ru>
2023-02-09 22:31:29 +0300

    All done from merge

--- load_env.py
+++ load_env.py
@@ -1,12 +1,12 @@
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("info.env")
 
 API_TOKEN = getenv("API_TOKEN", "")
-if API_TOKEN is "":
+if API_TOKEN == "":
     raise ValueError("API_TOKEN is not set")
 
 GOOGLE_TOKEN = getenv("GOOGLE_TOKEN", "")
-if GOOGLE_TOKEN is "":
+if GOOGLE_TOKEN == "":
     raise ValueError("GOOGLE_TOKEN is not set")
